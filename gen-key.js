1f6202b183ff85665bfee7a542ce830fc10e5d83
Vee <vee.jirapong@gmail.com>
2021-11-04 00:35:48 +0700

    Add gen keys

---
+++ gen-key.js
@@ -0,0 +1,29 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const random = require('random')
+const { SymbologyType, EncodingMode, createFile } = require('symbology')
+
+if (argv.quota === undefined)
+  return;
+
+let digit = 7;  
+let codes = [];
+while (codes.length < argv.quota) {
+  let code = random.int(0, Math.pow(10, digit) - 1).toString().padStart(digit, '0');
+  if (codes.indexOf(code) == -1) {
+    codes.push(code);
+  }
+}
+console.log(codes)
+
+codes.forEach((code) => {
+  createFile({
+    symbology: SymbologyType.UPCA,
+    fileName: `${__dirname}/keys/0000${code}.png`,
+    backgroundColor: 'FFFFFFFF',
+    foregroundColor: '00000000'
+  }, `${code}`).then(res => {
+    // console.log('Result: ', res)
+  })
+})
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const sendKeys = require('sendkeys-macos');
 const fs = require('fs');
 const { exec } = require("child_process");
+const moment = require('moment');
 
 const pattern = /\d{12}/;
 const readline = require('readline');
@@ -24,18 +25,23 @@ process.stdin.on('keypress', (str, key) => {
   }, 100);
 
   if (pattern.test(input)) {
-    fs.writeFile(`./keys/${input}.key`, '', function (err) {
-      if (err) throw err;
-    });
+    code = input.substring(0, input.length - 1);
+    file = `${__dirname}/keys/${code}.png`;
+    if (fs.existsSync(file)) {
+      let dir = `${__dirname}/keys/${moment(new Date()).format('YYYY-MM-DD')}`;
+      fs.mkdir(dir, _ => {
+        fs.rename(file, `${dir}/${code}.png`, _ => {
+          console.log(`code: "${input}"`);
+          input = '';
+          clearTimeout(exec);
+          sendKeys('Photo Booth', '<c:return>', { delay: 0.1, initialDelay: 1 });
 
-    console.log(`You pressed "${input}"`);
-    input = '';
-    clearTimeout(exec);
-    sendKeys('Photo Booth', '<c:return>', { delay: 0.1, initialDelay: 1 });
-
-    setTimeout(() => {
-      sendKeys('Terminal', `cd ${__dirname} <c:return>node index\.js <c:return>`, { delay: 0, initialDelay: 0 });
-      process.exit();
-    }, appDuration);
+          setTimeout(() => {
+            sendKeys('Terminal', `cd ${__dirname} <c:return>node index\.js <c:return>`, { delay: 0, initialDelay: 0 });
+            process.exit();
+          }, appDuration);
+        });
+      });
+    }
   }
 });
